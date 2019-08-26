00bfb6b5caff4efdf8b488464d799ef4ed2aaf9c
romevaf <fvarrom@gmail.com>
2019-08-26 11:40:23 -0500

    inputLocation meta data job dataflow

--- src/app/shared/services/data-prep.service.ts
+++ src/app/shared/services/data-prep.service.ts
@@ -30,11 +30,13 @@ export class DataPrepService {
   }
 
   public getFiles(authtoken: string, jobName: string, inputLocations?: string, outputLocations?: string): Observable<any> {
+    const outputLocations2 = "gs://dataprep-staging-e6fbd213-f292-4f5a-a1f2-ecb2b4529d6a/fvargas@belatrixsf.com/OUTPUTS/profilerTypeCheckHistograms.json/file";
+    const outputLocations3 = "gs://dataprep-staging-e6fbd213-f292-4f5a-a1f2-ecb2b4529d6a/fvargas@belatrixsf.com/OUTPUTS/profilerValidValueHistograms.json/file";
     this.resource.jobName = jobName;
     this.resource.parameters.inputLocations =
-    `{\"location1\":\"${inputLocations}\", \"location2\":\"${inputLocations}\", \"location3\":\"${inputLocations}\"}`;
+      `{\"location1\":\"${inputLocations}\", \"location2\":\"${inputLocations}\", \"location3\":\"${inputLocations}\"}`;
     this.resource.parameters.outputLocations =
-    `{\"location1\":\"${outputLocations}\",\"location2\":\"${outputLocations}\",\"location3\":\"${outputLocations}\"}`;
+      `{\"location1\":\"${outputLocations}\",\"location2\":\"${outputLocations2}\",\"location3\":\"${outputLocations3}\"}`;
 
     return this.httpClient.post(this.API_URL, this.resource, {
       headers: new HttpHeaders({
